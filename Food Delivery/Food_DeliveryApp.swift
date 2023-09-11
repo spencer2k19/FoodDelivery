bcc3262002343fb8d000f96fa4ddf58a7df3709b
spencer2k19 <hachemeloic@gmail.com>
2023-09-11 11:41:52 +0100

    Initial Commit

---
+++ Food Delivery/Food_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DeliveryApp.swift
+//  Food Delivery
+//
+//  Created by Loic HACHEME on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Food_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
